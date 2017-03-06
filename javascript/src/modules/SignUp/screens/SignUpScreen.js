4ef74f702f5df99ee42ac92c28c38a01b8f60437
Abbas Ali <aali@grepruby.com>
2017-03-06 12:57:21 +0530

    UI fixes

--- javascript/src/modules/SignUp/screens/SignUpScreen.js
+++ javascript/src/modules/SignUp/screens/SignUpScreen.js
@@ -9,7 +9,8 @@ import {
   TouchableOpacity,
   Image,
   Dimensions,
-  Alert
+  Alert,
+  KeyboardAvoidingView,
 } from 'react-native';
 
 import SignUpActions from '../actions';
@@ -74,7 +75,7 @@ export class SignUp extends Component {
 
   _handleDatePicked = (date) => {
     this.props.setFormValue("dob", date);
-    this.refs.number.focus();
+    this.refs.email.focus();
     this._hideDateTimePicker();
   };
 
@@ -117,113 +118,122 @@ export class SignUp extends Component {
         style={{ flex: 1, backgroundColor: 'grey', padding: 10, paddingTop: 64, height: null, width: null }}
         source={require('../../../../assets/images/bg2.jpg')}
       >
-        <View style={{ flex: 0.4, justifyContent: 'center', alignItems: 'center' }}>
-          <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
-            <View style={[styles.avatar, styles.avatarContainer, {marginBottom: 20}]}>
-            { this.props.image === null ? <Image
-                  source={require('../../../../assets/images/name.png')}
-                /> :
-              <Image style={styles.avatar} source={this.props.image} />
-            }
-            </View>
-          </TouchableOpacity>
-        </View>
-        <View style={{ flex: 1 }}>
-          <TextField
-            style={styles.textFieldStyle}
-            label={'Name'}
-            iconClass={FontAwesomeIcon}
-            iconName={'user'}
-            iconColor={'steelblue'}
-            value={this.props.name}
-            onChangeText={(text) => {
-              this.props.setFormValue("name", text);
-            }}
-          />
-          <TextField
-            style={styles.textFieldStyle}
-            label={'Email'}
-            iconClass={FontAwesomeIcon}
-            iconName={'envelope'}
-            iconColor={'steelblue'}
-            value={this.props.email}
-            keyboardType="email-address"
-            onChangeText={(text) => {
-              this.props.setFormValue("email", text);
-            }}
-          />
-          <TextField
-            style={styles.textFieldStyle}
-            label={'DOB'}
-            iconClass={FontAwesomeIcon}
-            iconName={'calendar'}
-            iconColor={'steelblue'}
-            onFocus={this._showDateTimePicker}
-            value={this.props.dob === null ? this.props.dob : this.props.dob.toLocaleString().split(',')[0]}
-          />
-          <TextField
-            ref='number'
-            style={styles.textFieldStyle}
-            label={'Phone Number'}
-            iconClass={FontAwesomeIcon}
-            value={this.props.number}
-            iconName={'phone'}
-            iconColor={'steelblue'}
-            keyboardType="numeric"
-            onChangeText={(text) => {
-              this.props.setFormValue("number", text);
-            }}
-          />
-          <View style={[{ flexDirection: 'row', justifyContent: 'space-around' }, styles.textFieldStyle]}>
-            <TouchableOpacity
-              style={{ justifyContent: 'center', alignItems: 'center' }}
-              onPress={() => {
-                this.props.setFormValue("male", true);
-              }}            >
-              <Image
-                source={this.props.male ? require('../../../../assets/images/male_filled.png') : require('../../../../assets/images/male.png')}
-              />
-              <Text>Male</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{ justifyContent: 'center', alignItems: 'center' }}
-              onPress={() => {
-                this.props.setFormValue("male", false);
-              }}
-            >
-              <Image
-                source={!this.props.male ? require('../../../../assets/images/female_filled.png') : require('../../../../assets/images/female.png')}
-              />
-              <Text>Female</Text>
-            </TouchableOpacity>
-
-          </View>
-          <SignUpButton
-            containerStyles={styles.signUpBottomStyles}
-            color='white' title={' Sign Up '}
-            onPress={() => {
-              const testEmail =  /^[ ]*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})[ ]*$/i;
-              if(this.props.image !== null && this.props.name !== "" && this.props.email !== "" && testEmail.test(this.props.email) && this.props.number !== null && this.props.dob !== null) {
-                Actions.ProfileScreen();
-              } else {
-                Alert.alert(
-                  'Error',
-                  'Please fill all the fields correctly',
-                  [
-                    {text: 'OK', onPress: () => console.log('OK Pressed')},
-                  ],
-                  { cancelable: false }
-                )
+        <KeyboardAvoidingView behavior='height' style={{ flex: 1 }}>
+          <View style={{ flex: 0.4, justifyContent: 'center', alignItems: 'center' }}>
+            <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
+              <View style={[styles.avatar, styles.avatarContainer, {marginBottom: 20}]}>
+              { this.props.image === null ? <Image
+                    source={require('../../../../assets/images/name.png')}
+                  /> :
+                <Image style={styles.avatar} source={this.props.image} />
               }
-            }}
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={{ flex: 1 }}>
+            <TextField
+              style={styles.textFieldStyle}
+              label={'Name'}
+              iconClass={FontAwesomeIcon}
+              iconName={'user'}
+              iconColor={'steelblue'}
+              value={this.props.name}
+              onChangeText={(text) => {
+                this.props.setFormValue("name", text);
+              }}
+              onSubmitEditing={() => this.refs.number.focus()}
+              returnKeyType='next'
+            />
+            <TextField
+              ref='number'
+              style={styles.textFieldStyle}
+              label={'Phone Number'}
+              iconClass={FontAwesomeIcon}
+              value={this.props.number}
+              iconName={'phone'}
+              iconColor={'steelblue'}
+              keyboardType="numeric"
+              returnKeyType='next'
+              onSubmitEditing={() => this.refs.dob.focus()}
+              onChangeText={(text) => {
+                this.props.setFormValue("number", text);
+              }}
+            />
+            <TextField
+              ref='dob'
+              style={styles.textFieldStyle}
+              label={'DOB'}
+              iconClass={FontAwesomeIcon}
+              iconName={'calendar'}
+              iconColor={'steelblue'}
+              onFocus={this._showDateTimePicker}
+              value={this.props.dob === null ? this.props.dob : this.props.dob.toLocaleString().split(',')[0]}
+            />
+            <TextField
+              ref='email'
+              style={styles.textFieldStyle}
+              label={'Email'}
+              iconClass={FontAwesomeIcon}
+              iconName={'envelope'}
+              iconColor={'steelblue'}
+              value={this.props.email}
+              keyboardType="email-address"
+              returnKeyType='next'
+              onChangeText={(text) => {
+                this.props.setFormValue("email", text);
+              }}
+            />
+            <View style={[{ flexDirection: 'row', justifyContent: 'space-around' }, styles.textFieldStyle]}>
+              <TouchableOpacity
+                style={{ justifyContent: 'center', alignItems: 'center' }}
+                onPress={() => {
+                  this.props.setFormValue("male", true);
+                }}            >
+                <Image
+                  source={this.props.male ? require('../../../../assets/images/male_filled.png') : require('../../../../assets/images/male.png')}
+                />
+                <Text>Male</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{ justifyContent: 'center', alignItems: 'center' }}
+                onPress={() => {
+                  this.props.setFormValue("male", false);
+                }}
+              >
+                <Image
+                  source={!this.props.male ? require('../../../../assets/images/female_filled.png') : require('../../../../assets/images/female.png')}
+                />
+                <Text>Female</Text>
+              </TouchableOpacity>
+
+            </View>
+            <SignUpButton
+              containerStyles={styles.signUpBottomStyles}
+              color='white' title={' Sign Up '}
+              onPress={() => {
+                const testEmail =  /^[ ]*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})[ ]*$/i;
+                if(this.props.image !== null && this.props.name !== "" && this.props.email !== "" && testEmail.test(this.props.email) && this.props.number !== null && this.props.dob !== null) {
+                  Actions.ProfileScreen();
+                } else {
+                  Alert.alert(
+                    'Error',
+                    'Please fill all the fields correctly',
+                    [
+                      {text: 'OK', onPress: () => console.log('OK Pressed')},
+                    ],
+                    { cancelable: false }
+                  )
+                }
+              }}
+            />
+          </View>
+          <DateTimePicker
+            isVisible={this.state.isDateTimePickerVisible}
+            onConfirm={this._handleDatePicked}
+            onCancel={this._hideDateTimePicker}
+            maximumDate={new Date}
           />
-        </View>
-        <DateTimePicker
-          isVisible={this.state.isDateTimePickerVisible}
-          onConfirm={this._handleDatePicked}
-          onCancel={this._hideDateTimePicker}
-          maximumDate={new Date}
-        />
+        </KeyboardAvoidingView>
       </Image>
     );
   }
